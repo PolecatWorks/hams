bbe80812d01dac68c8a140478dac6a70859ec333
Ben Greene <BenJGreene@gmail.com>
2022-10-20 23:38:47 +0100

    Add panic wrapper around ffi functions and check in tests:w Z

--- src/hams.rs
+++ src/hams.rs
@@ -1,8 +1,8 @@
-/// A UService allowsing multiple pServices to be assembled within it.
-/// The UService provides the basic scaffolding for the web service and a loader capabilty to load and service picoservices.
-/// The basic UService will reply with status information on the picoservcies provided
+/// A HaMS provides essential facilities to support a k8s microservice.
+/// health, liveness, startup, shutdown, monitoring, logging
 #[derive(Debug)]
 pub struct Hams {
+    /// A HaMS has a nmae which is used for distinguishing it on APIs
     pub name: String,
     // pub rt: tokio::runtime::Runtime,
     // channels: Arc<Mutex<Vec<mpsc::Sender<()>>>>,
@@ -11,11 +11,18 @@ pub struct Hams {
     // liveness: HealthCheck,
     // readyness: HealthCheck,
     // kill: Option<Mutex<Sender<()>>>,
+    /// Provide the version of the release of HaMS
     version: String,
+    /// Provide the port on which to serve the HaMS readyness and liveness
     port: u16,
 }
 
 impl<'a> Hams {
+    /// Returns a HaMS instance with the name given
+    ///
+    /// # Arguments
+    ///
+    /// * 'name' - A string slice that holds the name of the HaMS
     pub fn new(name: &str) -> Hams {
         Hams {
             name: name.to_string(),
