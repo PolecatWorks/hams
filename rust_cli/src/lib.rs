7503e7d45d682991981e2190bc253f92a3135657
Ben Greene <BenJGreene@gmail.com>
2022-10-27 08:13:21 +0100

    Add deaccloc to Drop

--- rust_cli/src/lib.rs
+++ rust_cli/src/lib.rs
@@ -50,44 +50,14 @@ impl<'a> Hams<'a> {
     }
 }
 
-// /**
-//  * Create a HaMS instance
-//  */
-// pub fn hams_init_ffi<S: Into<String>>(name: S) -> Result<Hams, HamsError>
-// where
-//     S: Display,
-// {
-//     info!("Registering HaMS: {}", &name);
-//     let c_name = std::ffi::CString::new(name.into())?;
-
-//     // if reply from function is null then reply with error
-//     let hams = unsafe { ffi::hams_init(c_name.as_ptr()) };
-//     if hams == ptr::null_mut() {
-//         return Err(HamsError::Message("Null reply from registering".to_owned()));
-//     }
-//     Ok(unsafe {*hams})
-// }
-
-/**
- * Deregister the shared library
- */
-pub fn hams_free_ffi(mut library: ffi::Hams) -> Result<(), HamsError> {
-    // change return type to be Result so that we can capture error
-    let retval = unsafe { ffi::hams_free(&mut library) };
-    if retval != 0 {
-        Err(HamsError::Message("Failed to freem hams".to_string()))
-    } else {
-        Ok(())
-    }
-}
-
-/// Start the HaMS http service
-pub fn hams_start_ffi(library: &mut ffi::Hams) -> Result<(), HamsError> {
-    let retval = unsafe { ffi::hams_start(library) };
-    if retval != 0 {
-        Err(HamsError::Message("Failed to start HaMS".to_string()))
-    } else {
-        Ok(())
+impl<'a> Drop for Hams<'a> {
+    /// Releaes the HaMS ffi on drop
+    fn drop(&mut self) {
+        let retval = unsafe { ffi::hams_free(self.c) };
+        if retval == 0 {
+            panic!("FAILED to freem HaMS");
+        }
+        info!("HaMS freed")
     }
 }
 
