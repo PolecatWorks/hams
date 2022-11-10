f024cb16c36806644c7bde2a64e58ec63efbb33a
Ben Greene <BenJGreene@gmail.com>
2022-11-10 23:55:36 +0000

    Add structs

--- sample-kotlin/src/main/kotlin/com/polecatworks/kotlin/samples/Hello.kt
+++ sample-kotlin/src/main/kotlin/com/polecatworks/kotlin/samples/Hello.kt
@@ -1,5 +1,5 @@
 package com.polecatworks.kotlin.samples
-import com.sun.jna.Native
+import com.sun.jna.*
 
 fun main(args: Array<String>) {
     println("Hello, World")
@@ -9,6 +9,17 @@ fun main(args: Array<String>) {
     ben.testMe(3)
     val INSTANCE = Native.load("hams", CHams::class.java)
     INSTANCE.hello_world()
+
+    val ben2 = INSTANCE.hello_node()
+
+    println("node is $ben2")
     // val ben2 = ben.foo(2);
     // println("Got a $ben2 as reply")
+
+    var myRustStr = RustStr(Pointer(1),NativeLong(2L))
+    println("RustStr = ${myRustStr.size()}")
+
+
+    var myRustString = RustString(Pointer(1),NativeLong(2L), NativeLong(3))
+    println("RustString = ${myRustString.size()}")
 }
--- sample-kotlin/src/main/kotlin/com/polecatworks/kotlin/samples/hams.kt
+++ sample-kotlin/src/main/kotlin/com/polecatworks/kotlin/samples/hams.kt
@@ -1,15 +1,55 @@
 
 
 package com.polecatworks.kotlin.samples
-import com.sun.jna.Library
-import com.sun.jna.Native
+import com.sun.jna.*
 
 // https://www.baeldung.com/java-jna-dynamic-libraries
+// https://java-native-access.github.io/jna/3.5.0/javadoc/index.html
+
+
+class RustStr (
+    @JvmField var ptr: Pointer,
+    @JvmField var len: NativeLong,
+): Structure(Structure.ALIGN_GNUC) {
+    override fun getFieldOrder() = listOf("ptr", "len",)
+}
+
+class RustString (
+    @JvmField var ptr: Pointer,
+    @JvmField var cap: NativeLong,
+    @JvmField var len: NativeLong,
+): Structure(Structure.ALIGN_GNUC) {
+    override fun getFieldOrder() = listOf("ptr", "cap", "len",)
+}
+
+
+class ExternCMetadata (
+    @JvmField var level: NativeLong,
+    @JvmField var target: RustStr,
+): Structure(Structure.ALIGN_GNUC) {
+    override fun getFieldOrder() = listOf("level", "target")
+}
+
+
+
+class ExternCRecord (
+    @JvmField var metadata: ExternCMetadata,
+    @JvmField var message: RustString,
+    @JvmField var module_path: RustStr,
+    @JvmField var file: RustStr,
+    @JvmField var line: NativeLong,
+): Structure(Structure.ALIGN_GNUC) {
+    override fun getFieldOrder() = listOf("metadata", "message", "module_path", "file","line")
+}
+
+
 
 interface CHams : Library {
     // CHams INSTANCE = Native.load("hams", CHams))
 
-    fun hello_world();
+    fun hello_world()
+    fun hello_node(): Int
+    // fun hams_logger_init(param: LogParam)
 }
 
 
@@ -26,9 +66,6 @@ class HamsJni constructor() {
     }
 
     external fun foo(x: Int): Double
-    //public native fun stringFromJNI(): String
-
-    //native fun unimplementedStringFromJNI(): String
 
     companion object {
         init {
