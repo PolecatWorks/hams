6f2efbdf661ee809438aa713de11be2e4f2cc3f1
Ben Greene <BenJGreene@gmail.com>
2022-11-14 22:22:05 +0000

    Working verison of JDK17 using incubator modules

---
+++ sample-kotlin/src/main/java/MyJava.java
@@ -0,0 +1,8 @@
+public class MyJava {
+    public void howdy() {
+        System.out.println("Howdy from JAVA");
+    }
+    public static void main(String[] args) {
+        System.out.println("HELLO from JAVA");
+    }
+}
--- sample-kotlin/src/main/kotlin/com/polecatworks/kotlin/samples/Hello.kt
+++ sample-kotlin/src/main/kotlin/com/polecatworks/kotlin/samples/Hello.kt
@@ -1,26 +1,31 @@
 package com.polecatworks.kotlin.samples
 import com.sun.jna.*
+import MyJava
 
 fun main(args: Array<String>) {
     println("Hello, World")
 
+    var myJava = MyJava()
+    myJava.howdy()
 
-    val ben = HamsJni()
-    ben.testMe(3)
-    val INSTANCE = Native.load("hams", CHams::class.java)
-    INSTANCE.hello_world()
+//     val ben = HamsJni()
+    // ben.testMe(3)
+//     val instance = Native.load("hams", CHams::class.java)
+//     instance.hello_world()
 
-    val ben2 = INSTANCE.hello_node()
+    // val ben2 = INSTANCE.hello_node()
 
-    println("node is $ben2")
-    // val ben2 = ben.foo(2);
-    // println("Got a $ben2 as reply")
+    // println("node is $ben2")
+    // // val ben2 = ben.foo(2);
+    // // println("Got a $ben2 as reply")
 
-    var myRustStr = RustStr(Pointer(1),NativeLong(2L))
-    println("RustStr = ${myRustStr.size()}")
+    // var myRustStr = RustStr(Pointer(1),NativeLong(2L))
+    // println("RustStr = ${myRustStr.size()}")
 
-    var myHamsForeign = HamsForeign()
+     var myHamsForeign = HamsForeign()
 
-    var myRustString = RustString(Pointer(1),NativeLong(2L), NativeLong(3))
-    println("RustString = ${myRustString.size()}")
+    myHamsForeign.checkMeOut()
+
+    // var myRustString = RustString(Pointer(1),NativeLong(2L), NativeLong(3))
+    // println("RustString = ${myRustString.size()}")
 }
