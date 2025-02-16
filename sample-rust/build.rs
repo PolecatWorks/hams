d0331367384f8a3eb4d9869d4eba42fde09989a4
Ben Greene <BenJGreene@gmail.com>
2025-02-16 19:53:30 +0000

    Correct build.rs to point to lib

--- sample-rust/build.rs
+++ sample-rust/build.rs
@@ -2,12 +2,12 @@ use std::env;
 use std::path::Path;
 
 fn main() {
-    let dir = env::var("CARGO_MANIFEST_DIR").unwrap();
+    let dir = env::var("OUT_DIR").unwrap();
+
+    let target_path = Path::new(&dir).join("../../..");
+
     #[cfg(target_os = "macos")]
-    println!(
-        "cargo:rustc-link-search=native={}",
-        Path::new(&dir).join("target/lib").display()
-    );
+    println!("cargo:rustc-link-search=native={}", target_path.display());
 
     // // From here: https://crates.io/crates/bind-builder BUT cannot get it working so using rustc-link-search instead. Followed by using install_name_tool as noted in README.md
     #[cfg(target_os = "macos")]
